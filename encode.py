9fddaa91446f08937f56eab1bc94cdc7c0f5d8b3
blokhinnv <darenne@yandex.ru>
2025-03-12 18:15:16 +0300

    compress_text func

--- encode.py
+++ encode.py
@@ -1,13 +1,25 @@
+import base64
+import zlib
+
 import keyboard
 import pyautogui
 import pyperclip
 from tqdm import tqdm
 
+
+def compress_text(text: str) -> str:
+    b64 = base64.b64encode(zlib.compress(text.encode(), level=9))
+    b64_text = b64.decode()
+    return b64_text
+
+
 if __name__ == "__main__":
     print("waiting for F10 to be pressed...")
+    print("don't forget to copy text into clipboard!")
     keyboard.wait("f10")
     text_to_type = pyperclip.paste()
-    for char in tqdm(text_to_type):
+    compressed_text = compress_text(text_to_type)
+    for char in tqdm(compress_text):
         pyautogui.typewrite(char)
 
     print("Text typed.")
