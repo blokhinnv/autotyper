8cb35c6de5d13c4ca360eea6bf906d7211d08bde
blokhinnv <darenne@yandex.ru>
2025-03-12 18:04:04 +0300

    fanstastic improvement

--- decode.py
+++ decode.py
@@ -1,32 +1,11 @@
+import base64
 import getpass
+import zlib
 from pathlib import Path
 
-qwerty = r"""`qwertyuiop[]asdfghjkl;'zxcvbnm,.~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>"""
-ycuken = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
 
-char_translation_dict = dict(zip(qwerty, ycuken))
-assert len(qwerty) == len(ycuken)
-
-ESCAPE_CHAR = "$"
-
-
-def translate_char(key: str) -> str:
-    return "".join(char_translation_dict.get(x, x) for x in key)
-
-
-def translate_text(text: str) -> str:
-    skip_next_translation = False
-    chars: list = []
-    for idx in range(len(text)):
-        c: str = text[idx]
-        if c != ESCAPE_CHAR and not skip_next_translation:
-            chars.append(translate_char(c))
-        elif c != ESCAPE_CHAR and skip_next_translation:
-            chars.append(c)
-            skip_next_translation = False
-        elif c == ESCAPE_CHAR:
-            skip_next_translation = True
-    return "".join(chars)
+def decode_text(text: str) -> str:
+    return zlib.decompress(base64.b64decode(text)).decode()
 
 
 if __name__ == "__main__":
@@ -39,7 +18,7 @@ if __name__ == "__main__":
 
     with open(postman_path / "in.txt", encoding="utf8") as fp:
         text: str = fp.read()
-        translated: str = translate_text(text)
+        decoded: str = decode_text(text)
         with open(postman_path / "out.txt", "w", encoding="utf8") as fp:
-            fp.write(translated)
-        print("package received and translated")
+            fp.write(decoded)
+        print("package received and decoded")
--- encode.py
+++ encode.py
@@ -3,50 +3,11 @@ import pyautogui
 import pyperclip
 from tqdm import tqdm
 
-qwerty = r"""`qwertyuiop[]asdfghjkl;'zxcvbnm,.~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>"""
-ycuken = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
-
-
-assert len(set(qwerty)) == len(qwerty)
-assert len(set(ycuken)) == len(ycuken)
-
-assert len(qwerty) == len(ycuken)
-# join as keys and values
-tr = dict(zip(ycuken, qwerty))
-
-ESCAPE_CHAR = "$"
-
-
-def translate(key: str) -> str:
-    """Returns qwerty key or the given key itself if no mapping found"""
-    return "".join(map(lambda x: tr.get(x, x), key))
-
-
-def escape_char(c: str) -> str:
-    return f"{ESCAPE_CHAR}{c}"
-
-
-def escape_eng_chars(s: str) -> str:
-    r = []
-    for c in s:
-        if c in ycuken:
-            r.append(translate(c))
-        elif c == "\n":
-            r.append(c)
-        else:
-            r.append(escape_char(c))
-    return "".join(r)
-
-
 if __name__ == "__main__":
-    print("waiting for caps lock to be pressed...")
+    print("waiting for F10 to be pressed...")
     keyboard.wait("f10")
     text_to_type = pyperclip.paste()
-    # Simulate typing each character of the text
-    text_to_type = escape_eng_chars(text_to_type)
     for char in tqdm(text_to_type):
-        char = translate(char)
         pyautogui.typewrite(char)
 
-    # keyboard.press("ctrl+s")
     print("Text typed.")
